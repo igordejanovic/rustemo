a4da089ac3c792c3e7acafd0a26455256b277464
Igor Dejanovic <igor.dejanovic@gmail.com>
2022-06-13 10:11:13 +0200

    Comment out unused code

--- src/grammar.rs
+++ src/grammar.rs
@@ -3,75 +3,75 @@ use std::hash::Hash;
 
 pub(crate) type Priority = u8;
 
-#[derive(Debug)]
-struct Recognizer;
+// #[derive(Debug)]
+// struct Recognizer;
 
-#[derive(Debug)]
-pub enum Symbol<'a> {
-    Terminal(Terminal),
-    NonTerminal(NonTerminal<'a>),
-}
+// #[derive(Debug)]
+// pub enum Symbol<'a> {
+//     Terminal(Terminal),
+//     NonTerminal(NonTerminal<'a>),
+// }
 
-impl<'a> Hash for Symbol<'a> {
-    fn hash<H>(&self, state: &mut H)
-    where
-        H: std::hash::Hasher,
-    {
-        match self {
-            Symbol::Terminal(t) => t.name.hash(state),
-            Symbol::NonTerminal(n) => n.name.hash(state),
-        }
-    }
-}
-
-/// Represent a terminal symbol of the grammar.
-#[derive(Debug, Default)]
-pub struct Terminal {
-    pub name: &'static str,
-    pub fqn: &'static str,
-    pub location: Option<Location>,
-
-    /// Priority used for lexical disambiguation.
-    pub prior: Priority,
-
-    /// Used for scanning optimization. If this terminal is `finish` no other
-    /// recognizers will be checked if this succeeds. If not provided in the
-    /// grammar implicit rules will be used during table construction.
-    pub finish: bool,
-
-    /// Prefer this recognizer in case of multiple recognizers match at the same
-    /// place and implicit disambiguation doesn't resolve.
-    pub prefer: bool,
-    // /// Should dynamic disambiguation be called to resolve conflict involving
-    // /// this terminal.
-    // pub dynamic: bool,
-
-    // /// `true` if this Terminal represents keyword. `false` by default.
-    // pub keyword: bool,
-}
-
-// impl Symbol for Terminal {
-//     fn as_symbolbase(&self) -> &SymbolBase {
-//         &self.base
+// impl<'a> Hash for Symbol<'a> {
+//     fn hash<H>(&self, state: &mut H)
+//     where
+//         H: std::hash::Hasher,
+//     {
+//         match self {
+//             Symbol::Terminal(t) => t.name.hash(state),
+//             Symbol::NonTerminal(n) => n.name.hash(state),
+//         }
 //     }
 // }
 
-#[derive(Debug, Default)]
-pub struct NonTerminal<'a> {
-    pub name: &'static str,
-    pub fqn: &'static str,
-    pub location: Option<Location>,
+// /// Represent a terminal symbol of the grammar.
+// #[derive(Debug, Default)]
+// pub struct Terminal {
+//     pub name: &'static str,
+//     pub fqn: &'static str,
+//     pub location: Option<Location>,
 
-    /// Productions which define this non-terminal rule.
-    pub productions: Vec<&'a Production<'a>>,
-}
+//     /// Priority used for lexical disambiguation.
+//     pub prior: Priority,
 
-#[derive(Debug)]
-pub struct Production<'a> {
-    pub id: usize,
-    pub lhs: &'a NonTerminal<'a>,
-    pub rhs: Vec<&'a Symbol<'a>>,
-}
+//     /// Used for scanning optimization. If this terminal is `finish` no other
+//     /// recognizers will be checked if this succeeds. If not provided in the
+//     /// grammar implicit rules will be used during table construction.
+//     pub finish: bool,
+
+//     /// Prefer this recognizer in case of multiple recognizers match at the same
+//     /// place and implicit disambiguation doesn't resolve.
+//     pub prefer: bool,
+//     // /// Should dynamic disambiguation be called to resolve conflict involving
+//     // /// this terminal.
+//     // pub dynamic: bool,
+
+//     // /// `true` if this Terminal represents keyword. `false` by default.
+//     // pub keyword: bool,
+// }
+
+// // impl Symbol for Terminal {
+// //     fn as_symbolbase(&self) -> &SymbolBase {
+// //         &self.base
+// //     }
+// // }
+
+// #[derive(Debug, Default)]
+// pub struct NonTerminal<'a> {
+//     pub name: &'static str,
+//     pub fqn: &'static str,
+//     pub location: Option<Location>,
+
+//     /// Productions which define this non-terminal rule.
+//     pub productions: Vec<&'a Production<'a>>,
+// }
+
+// #[derive(Debug)]
+// pub struct Production<'a> {
+//     pub id: usize,
+//     pub lhs: &'a NonTerminal<'a>,
+//     pub rhs: Vec<&'a Symbol<'a>>,
+// }
 
 pub type TerminalInfos<const T: usize> = [TerminalInfo; T];
 pub type TerminalsState<const T: usize, const S: usize> = [[Option<usize>; T]; S];
