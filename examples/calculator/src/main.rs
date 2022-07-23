21b54bdd68acf78f0c2d8cbba91445c946d51eb7
Igor Dejanovic <igor.dejanovic@gmail.com>
2022-07-23 16:56:29 +0200

    Calculator example WIP

--- examples/calculator/src/main.rs
+++ examples/calculator/src/main.rs
@@ -1,5 +1,31 @@
+use std::{path::PathBuf, process::exit};
+
 mod calculator;
+mod calculator_actions;
 
 fn main() {
-    println!("{:?}", calculator::CalculatorParser.parse("2 + 3".into()));
+    let grammar_file: PathBuf = [env!("CARGO_MANIFEST_DIR"),
+                                 "src", "calculator.rustemo"].iter().collect();
+    if let Err(e) = rustemo::generate_parser(grammar_file) {
+        eprintln!("{}", e);
+        exit(1);
+    }
+    // println!(
+    //     "{:?}",
+    //     calculator::CalculatorParser::default().parse_default("2 + 3".into())
+    // );
+}
+
+#[test]
+fn test_parse_1() {
+    let mut parser = calculator::CalculatorParser::default();
+    let ast = parser.parse_default("2 + 3 * 7 + 6 * 3".into());
+    assert_eq!(ast, 41);
+}
+
+#[test]
+fn test_parse_2() {
+    let mut parser = calculator::CalculatorParser::default();
+    let ast = parser.parse_default("2 + ( 3  * 7 )".into());
+    assert_eq!(ast, 35);
 }
