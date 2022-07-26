92b5f37b7a30223c99c84f2bd30022a9fe4fab8c
Igor Dejanovic <igor.dejanovic@gmail.com>
2022-07-26 13:06:28 +0200

    Fix parser generator dir traversal

--- rustemo/src/build/mod.rs
+++ rustemo/src/build/mod.rs
@@ -1,20 +1,22 @@
 use std::fs;
 use std::path::{Path, PathBuf};
 
-use crate::generator::GeneratorResult;
 use crate::generator::generate_parser;
+use crate::generator::GeneratorResult;
 
-fn visit_dirs<P, F>(dir: P, visitor: F) -> GeneratorResult
+fn visit_dirs<P>(
+    dir: P,
+    visitor: &dyn Fn(&PathBuf) -> GeneratorResult,
+) -> GeneratorResult
 where
     P: AsRef<Path>,
-    F: Fn(&PathBuf) -> GeneratorResult
 {
     if dir.as_ref().is_dir() {
         for entry in fs::read_dir(dir)? {
             let entry = entry?;
             let path = entry.path();
             if path.is_dir() {
-                visit_dirs(&path, &visitor)?;
+                visit_dirs(&path, visitor)?;
             } else {
                 match path.extension() {
                     Some(ext) if ext == "rustemo" => visitor(&path)?,
@@ -28,8 +30,14 @@ where
 
 /// Recurse into a given directory and generate all parsers for .rustemo grammar
 /// files.
-pub fn generate_parsers<P: AsRef<Path>>(dir: P) -> GeneratorResult {
-   visit_dirs(dir, |path| {
-       generate_parser(path)
-   })
+pub fn generate_parsers<P>(dir: P) -> GeneratorResult
+where
+    P: AsRef<Path> + std::fmt::Debug {
+
+    fn visitor(grammar: &PathBuf) -> GeneratorResult {
+        log!("Generating parser for grammar {:?}", grammar);
+        generate_parser(grammar)
+    }
+
+    visit_dirs(dir, &visitor)
 }
