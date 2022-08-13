8eea7877e318d14b4d0d38e6a1ecf5704382aa5f
Igor Dejanovic <igor.dejanovic@gmail.com>
2022-08-13 15:12:23 +0200

    Reformat build script

--- examples/calculator/build.rs
+++ examples/calculator/build.rs
@@ -5,7 +5,10 @@ use rustemo::settings::Settings;
 fn main() {
     let root_dir: PathBuf =
         [env!("CARGO_MANIFEST_DIR"), "src"].iter().collect();
-    if let Err(e) = rustemo::generate_parsers(root_dir, &Settings::default().with_force(true)) {
+    if let Err(e) = rustemo::generate_parsers(
+        root_dir,
+        &Settings::default().with_force(true),
+    ) {
         eprintln!("{}", e);
         exit(1);
     }
