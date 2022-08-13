08314fe8d55360857203268d61d5a1aadf94fff6
Igor Dejanovic <igor.dejanovic@gmail.com>
2022-08-13 14:16:27 +0200

    Build script to regenerate parsers for tests

--- tests/build.rs
+++ tests/build.rs
@@ -1,3 +1,15 @@
-fn main() {
+use std::{path::PathBuf, process::exit};
 
+use rustemo::settings::Settings;
+
+fn main() {
+    let root_dir: PathBuf =
+        [env!("CARGO_MANIFEST_DIR"), "src"].iter().collect();
+    if let Err(e) = rustemo::generate_parsers(
+        root_dir,
+        &Settings::default().with_force_all(true),
+    ) {
+        eprintln!("{}", e);
+        exit(1);
+    }
 }
