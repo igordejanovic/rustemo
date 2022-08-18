c7ad03a1d5615f06623443fa32b3755e3ac211bd
Igor Dejanovic <igor.dejanovic@gmail.com>
2022-08-18 16:31:19 +0200

    Extended/improved zero-or-more tests

--- tests/src/sugar/zero_or_more/mod.rs
+++ tests/src/sugar/zero_or_more/mod.rs
@@ -2,9 +2,13 @@ use rustemo::output_cmp;
 use rustemo_rt::rustemo_mod;
 
 use self::zero_or_more_1::ZeroOrMore1Parser;
+use self::zero_or_more_2::ZeroOrMore2Parser;
 
 rustemo_mod!(zero_or_more_1, "/src/sugar/zero_or_more");
 rustemo_mod!(zero_or_more_1_actions, "/src/sugar/zero_or_more");
+
+rustemo_mod!(zero_or_more_2, "/src/sugar/zero_or_more");
+rustemo_mod!(zero_or_more_2_actions, "/src/sugar/zero_or_more");
 
 #[test]
 fn zero_or_more_1_1() {
@@ -47,6 +51,42 @@ fn zero_or_more_1_2_error() {
     let result = ZeroOrMore1Parser::parse_str("c b b a a a a");
     output_cmp!(
         "src/sugar/zero_or_more/zero_or_more_1_2_error.ast",
+        format!("{result:#?}")
+    );
+}
+
+#[test]
+fn zero_or_more_2_1() {
+    let result = ZeroOrMore2Parser::parse_str("c 1 2 3 a");
+    output_cmp!(
+        "src/sugar/zero_or_more/zero_or_more_2_1.ast",
+        format!("{result:#?}")
+    );
+}
+
+#[test]
+fn zero_or_more_2_2() {
+    let result = ZeroOrMore2Parser::parse_str("c a");
+    output_cmp!(
+        "src/sugar/zero_or_more/zero_or_more_2_2.ast",
+        format!("{result:#?}")
+    );
+}
+
+#[test]
+fn zero_or_more_2_1_error() {
+    let result = ZeroOrMore2Parser::parse_str("c c a");
+    output_cmp!(
+        "src/sugar/zero_or_more/zero_or_more_2_1_error.ast",
+        format!("{result:#?}")
+    );
+}
+
+#[test]
+fn zero_or_more_2_2_error() {
+    let result = ZeroOrMore2Parser::parse_str("c 1 2 a a");
+    output_cmp!(
+        "src/sugar/zero_or_more/zero_or_more_2_2_error.ast",
         format!("{result:#?}")
     );
 }
