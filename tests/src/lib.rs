1cd3f9df360420012fc3fcb3fbf3e5eb909424e7
Igor Dejanovic <igor.dejanovic@gmail.com>
2022-09-04 20:05:01 +0200

    Tidy dependencies

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,10 +1,17 @@
 #![allow(unused_imports)]
+#[cfg(test)]
 mod ambiguity;
+#[cfg(test)]
 mod layout;
+#[cfg(test)]
 mod partial;
+#[cfg(test)]
 mod pass_context;
+#[cfg(test)]
 mod rule_patterns;
+#[cfg(test)]
 mod sugar;
 
 // For output_cmp macro
+#[cfg(test)]
 pub use rustemo::utils::string_difference;
