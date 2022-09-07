5b7b0d1ba7aa1a925e926932e6f9d9d8d4cf3b6d
Igor Dejanovic <igor.dejanovic@gmail.com>
2022-09-07 13:19:54 +0200

    Initial impl of custom lexer

--- tests/src/custom_lexer/custom_lexer_lexer.rs
+++ tests/src/custom_lexer/custom_lexer_lexer.rs
@@ -1,4 +1,5 @@
-use rustemo_rt::{lexer::{Context, Lexer, Token}, index::StateIndex, location::Location};
+use rustemo_rt::{lexer::{self, Context, Lexer, Token, Input}, index::StateIndex, location::Location};
+use super::custom_lexer::TokenKind;
 
 pub struct CustomLexerLexer();
 
@@ -8,16 +9,28 @@ impl CustomLexerLexer {
     }
 }
 
-impl Lexer for CustomLexerLexer {
-    fn next_token(&self, context: &mut Context<[u8], (), StateIndex>) -> Token<[u8]> {
+pub struct Bytes<'i>(&'i [u8]);
+
+impl Input for Bytes {
+}
+
+impl<'i> Lexer<Bytes<'i>, (), StateIndex, TokenKind> for CustomLexerLexer {
+    fn next_token(&self, context: &mut Context<Bytes<'i>, (), StateIndex>) -> Token<Bytes<'i>, TokenKind> {
+        let value;
+        let kind;
         let pos = context.position;
-        while context.input[pos] & 0b1000_0000 {
-            pos+=1;
+        if context.position >= context.input.0.len() {
+            value = &[];
+            kind = lexer::TokenKind::STOP;
+        } else {
+            while context.input.0[pos] & 0b1000_0000 {
+                pos+=1;
+            }
+            let value = context.input.0[context.position..=pos];
         }
-        let value = context.input[context.position..=pos];
 
         Token{
-            terminal: ,
+            kind: lexer::TokenKind::Kind(TokenKind::VarInt),
             value,
             location: Some(Location { start: context.position, end: Some(pos) }),
         }
