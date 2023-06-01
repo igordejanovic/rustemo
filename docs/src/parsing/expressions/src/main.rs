55f5b06761d54b8f2791918a8ebd713dac6b48d6
Igor Dejanovic <igor.dejanovic@gmail.com>
2023-06-01 12:37:41 +0200

    fix: remove unused actions import

--- docs/src/parsing/expressions/src/main.rs
+++ docs/src/parsing/expressions/src/main.rs
@@ -2,10 +2,7 @@ use expressions::ExpressionsParser;
 use rustemo::rustemo_mod;
 
 rustemo_mod! {#[allow(unused_imports)]
-              pub(crate) expressions, "/src"}
-
-rustemo_mod! {#[allow(dead_code)]
-              pub(crate) expressions_actions, "/src"}
+pub(crate) expressions, "/src"}
 
 fn main() {
     println!("{:#?}", ExpressionsParser::new().parse("3 + 2 + 1"));
