fb8085f905f173c4c540e1d6daa3041508a502f6
Igor Dejanovic <igor.dejanovic@gmail.com>
2023-10-08 12:23:03 +0200

    test: extending GLR build tests

--- tests/src/glr/build/mod.rs
+++ tests/src/glr/build/mod.rs
@@ -1,6 +1,6 @@
 use std::borrow::BorrowMut;
 
-use rustemo::rustemo_mod;
+use rustemo::{lr::builder::TreeBuilder, rustemo_mod, glr::gss::GssHead};
 use rustemo_compiler::output_cmp;
 
 rustemo_mod!(calc, "/src/glr/build");
@@ -10,20 +10,66 @@ use self::calc::CalcParser;
 use rustemo::parser::Parser;
 
 #[test]
-fn glr_tree_build() {
+fn glr_tree_build_default() {
     let forest = CalcParser::new().parse("1 + 4 * 9").unwrap();
     assert_eq!(forest.solutions(), 2);
 
     let mut builder = calc::DefaultBuilder::new();
     output_cmp!(
-        "src/glr/build/tree_build_1.ast",
+        "src/glr/build/tree_build_default_1.ast",
         format!(
             "{:#?}",
             forest.get_first_tree().unwrap().build(&mut builder)
         )
     );
     output_cmp!(
-        "src/glr/build/tree_build_2.ast",
+        "src/glr/build/tree_build_default_2.ast",
         format!("{:#?}", forest.get_tree(1).unwrap().build(&mut builder))
+    );
+}
+
+#[test]
+fn glr_tree_build_generic() {
+    let forest = CalcParser::new().parse("1 + 4 * 9").unwrap();
+    assert_eq!(forest.solutions(), 2);
+
+    let mut builder = TreeBuilder::new();
+    output_cmp!(
+        "src/glr/build/tree_build_generic_1.ast",
+        format!(
+            "{:#?}",
+            forest.get_first_tree().unwrap().build::<TreeBuilder<
+                '_,
+                str,
+                calc::ProdKind,
+                calc::TokenKind,
+            >, GssHead<
+                '_,
+                str,
+                calc::State,
+                calc::TokenKind,
+            >, calc::State>(
+                &mut builder
+            )
+        )
+    );
+    output_cmp!(
+        "src/glr/build/tree_build_generic_2.ast",
+        format!(
+            "{:#?}",
+            forest.get_tree(1).unwrap().build::<TreeBuilder<
+                '_,
+                str,
+                calc::ProdKind,
+                calc::TokenKind,
+            >, GssHead<
+                '_,
+                str,
+                calc::State,
+                calc::TokenKind,
+            >, calc::State>(
+                &mut builder
+            )
+        )
     );
 }
