9f6272babfe04673ecccca3b787a280900a21dca
Igor Dejanovic <igor.dejanovic@gmail.com>
2024-06-02 17:56:08 +0200

    docs: run latex->image script from mdbook

--- docs/build-latex-images.sh
+++ docs/build-latex-images.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 # Creates images from LaTeX files.
+
+# Run only for html renderer
+if [  $2 != 'html' ]; then
+    exit 1
+fi
+
 while IFS= read -d '' -r file ; do
     printf 'File found: %s\n' "$file"
     pdflatex -output-directory=$(dirname $file) $file 2>&1 >> ${file%.tex}.log
@@ -7,3 +13,7 @@ while IFS= read -d '' -r file ; do
     #convert -density 200 -alpha deactivate ${file%.tex}.pdf ${file%.tex}.png
     # rm ${file%.tex}.{log,pdf,aux}
 done < <(find . -iname '*.tex' -print0)
+
+# This is required for the script to be called as mdbook preprocessor
+# as we don't wan't actually to process md files.
+exit 1
