08a2fdc0acc4301ce9052e20193868890927739f
Igor Dejanovic <igor.dejanovic@gmail.com>
2024-11-06 14:43:08 +0100

    test: improve-check output of regression test

--- tests/src/glr/regressions/issue_16_subtract_overflow_panic/mod.rs
+++ tests/src/glr/regressions/issue_16_subtract_overflow_panic/mod.rs
@@ -1,4 +1,5 @@
 use rustemo::{rustemo_mod, Parser};
+use rustemo_compiler::output_cmp;
 
 rustemo_mod!(
     inline,
@@ -13,10 +14,14 @@ use self::inline::{DefaultBuilder, InlineParser};
 
 #[test]
 fn subtract_overflow() {
-    InlineParser::new()
+    let result = InlineParser::new()
         .parse("*ld 2")
         .unwrap()
         .get_first_tree()
         .unwrap()
         .build(&mut DefaultBuilder::new());
+    output_cmp!(
+        "src/glr/regressions/issue_16_subtract_overflow_panic/result.ast",
+        format!("{:#?}", result)
+    );
 }
