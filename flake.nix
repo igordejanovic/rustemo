b1686197b6a413edbada5817969bcc888ca901ce
Igor Dejanovic <igor.dejanovic@gmail.com>
2025-05-13 21:11:25 +0200

    chore: add src/doc to rust setup

--- flake.nix
+++ flake.nix
@@ -1,58 +1,72 @@
 {
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-		flake-utils.url = "github:numtide/flake-utils";
-		rust-overlay = {
-			url = "github:oxalica/rust-overlay";
-			inputs = {
-				nixpkgs.follows = "nixpkgs";
-			};
-		};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    mdbook-theme = {
+      url = "github:zjp-CN/mdbook-theme";
+      flake = false;
+    };
+  };
 
-		crane = {
-			url = "github:ipetkov/crane";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane, mdbook-theme }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
 
-		mdbook-theme = {
-			url = "github:zjp-CN/mdbook-theme";
-			flake = false;
-		};
-	};
+        rustStable = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "clippy" "rustfmt" "rust-docs" ];
+        };
 
-	outputs = { self, nixpkgs, flake-utils, rust-overlay, crane, mdbook-theme }:
-		flake-utils.lib.eachDefaultSystem (system:
-			let
-				overlays = [ (import rust-overlay) ];
-				pkgs = import nixpkgs {
-					inherit system overlays;
-				};
+        rustNightly = pkgs.rust-bin.nightly.latest.default.override {
+          extensions = [ "rust-src" "clippy" "rustfmt" "rust-docs" ];
+        };
 
-				rev = if (self ? rev) then self.rev else self.dirtyRev;
-				book = import ./docs {
-					inherit pkgs crane mdbook-theme;
-				};
-				rustemo = import ./. {
-					inherit crane pkgs rev;
-				};
-				shellPkgs = [ pkgs.cargo-nextest rustemo.packages.compiler ];
-			in
-			{
-				devShells.default = pkgs.mkShell {
-					buildInputs = book.buildInputs ++ rustemo.buildInputs ++ shellPkgs;
-				};
-				devShells.beta = pkgs.mkShell {
-					buildInputs = book.buildInputs ++ [ pkgs.rust-bin.beta.latest.default ] ++ shellPkgs;
-				};
-				devShells.nightly = pkgs.mkShell {
-					buildInputs = book.buildInputs ++ [ pkgs.rust-bin.nightly.latest.default ] ++ shellPkgs;
-				};
-				inherit (rustemo) checks;
-				packages = rustemo.packages // book.packages;
-			}
-		);
+        rev = if (self ? rev) then self.rev else self.dirtyRev;
+        book = import ./docs {
+          inherit pkgs crane mdbook-theme;
+        };
+        rustemo = import ./. {
+          inherit crane pkgs rev;
+        };
+        shellPkgs = [ pkgs.cargo-nextest rustemo.packages.compiler ];
+      in
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = book.buildInputs ++ rustemo.buildInputs ++ shellPkgs ++ [ rustStable ];
+            RUST_SRC_PATH = "${rustStable}/lib/rustlib/src/rust/library";
+            shellHook = ''
+            echo "Using STABLE Rust environment"
+          '';
+          };
+          devShells.beta = pkgs.mkShell {
+            buildInputs = book.buildInputs ++ [ pkgs.rust-bin.beta.latest.default ] ++ shellPkgs;
+          };
+          devShells.nightly = pkgs.mkShell {
+            buildInputs = book.buildInputs ++ [ rustNightly ] ++ shellPkgs;
+            RUST_SRC_PATH = "${rustNightly}/lib/rustlib/src/rust/library";
+            shellHook = ''
+            echo "Using NIGHTLY Rust environment"
+          '';
+          };
+          inherit (rustemo) checks;
+          packages = rustemo.packages // book.packages;
+        }
+    );
 
-	nixConfig = {
-		bash-prompt-prefix = "rustemo-dev:";
-	};
+  nixConfig = {
+    bash-prompt-prefix = "rustemo-dev:";
+  };
 }
