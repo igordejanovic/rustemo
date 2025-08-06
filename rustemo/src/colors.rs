43ddcede1609847487bc8661e8a33fc6a4034ed4
Igor Dejanovic <igor.dejanovic@gmail.com>
2025-08-06 16:32:02 +0200

    fix: no_color condition
    Seems that the logic is inverse.

--- rustemo/src/colors.rs
+++ rustemo/src/colors.rs
@@ -1,7 +1,7 @@
 use yansi::{Color::*, Condition, Style};
 
 static COLORS: Condition =
-    Condition::from(|| Condition::stderr_is_tty() && !Condition::no_color());
+    Condition::from(|| Condition::stderr_is_tty() && Condition::no_color());
 
 pub static LOG: Style = Green.whenever(COLORS);
 pub static LOG_BOLD: Style = Green.bold().whenever(COLORS);
