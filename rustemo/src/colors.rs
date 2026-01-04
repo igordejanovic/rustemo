1905c24e57d1aec4b8ccde0b3ac54c1a83923e1a
Igor Dejanovic <igor.dejanovic@gmail.com>
2026-01-04 21:48:36 +0100

    fix: avoid yansi tty check for WASM target
    This is a workaround for a yansi problem. See discussion in #27.

--- rustemo/src/colors.rs
+++ rustemo/src/colors.rs
@@ -1,6 +1,10 @@
 use yansi::{Color::*, Condition, Style};
 
+#[cfg(not(target_family = "wasm"))]
 static COLORS: Condition = Condition::from(|| Condition::stderr_is_tty() && Condition::no_color());
+
+#[cfg(target_family = "wasm")]
+static COLORS: Condition = Condition::NEVER;
 
 pub static LOG: Style = Green.whenever(COLORS);
 pub static LOG_BOLD: Style = Green.bold().whenever(COLORS);
